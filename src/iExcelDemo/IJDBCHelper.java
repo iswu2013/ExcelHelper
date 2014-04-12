ebc686ad9702d19bdffda3b82d594e20d863dcf1
wujinsong <1037216275@qq.com>
2014-04-12 15:50:26 +0800

    add src

---
+++ src/iExcelDemo/IJDBCHelper.java
@@ -0,0 +1,18 @@
+package iExcelDemo;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public interface IJDBCHelper {
+	/**
+	 * 获取连接
+	 * 
+	 * @return
+	 */
+	public abstract Connection getConnection(LinkEntity entity);
+
+	/**
+	 * 获取结果集
+	 * @return
+	 */
+	public abstract ResultSet getResultSet(Connection connection,String sqlString);
+}
